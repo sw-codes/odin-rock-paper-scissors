5f0d532e8b1fbcc909560b184d90d0224907448e
swcodes <87975571+sw-codes@users.noreply.github.com>
2025-02-27 11:40:13 +0000

    5 rounds implemented
    The game is implemented.
    Checks for a draw which counts as a round and removes the draw being a loss bug.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice() {
     computerNum = Math.ceil(Math.random() * 3);
     computerChoice = ""
@@ -23,5 +25,51 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-getComputerChoice();
-getHumanChoice();
+
+
+function playGame() {
+    let computerScore = 0;
+    let humanScore = 0;
+    let roundCount = 0;
+
+    while (roundCount < 5) {
+        playRound(getHumanChoice(), getComputerChoice());
+        roundCount++;
+    }
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+    } else {
+        console.log("The machines have won!");
+    }
+    console.log("Game over.");
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        //rock beats scissors
+        //paper beats rock
+        //scissors beats paper
+        if (humanChoice == computerChoice) {
+            console.log("It's a draw! Nobody wins.");
+            roundCount++;
+            playRound(getHumanChoice(), getComputerChoice());
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
+            humanScore++;
+            console.log("You win! Rock beats Scissors.");
+        } else if (humanChoice == "paper" && computerChoice == "rock") {
+            humanScore++;
+            console.log("You win! Paper beats Rock.");
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+            humanScore++;
+            console.log("You win! Scissors beats Paper.");
+        } else {
+            computerScore++;
+            console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+        }
+        
+    }
+}
+
+// getComputerChoice();
+// getHumanChoice();
+// playRound(getHumanChoice(), getComputerChoice());
+playGame();
